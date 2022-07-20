e28e41ed053ceb163e1118629bdc55621aa5ac50
Jackeline <jackelineandrea58@hotmail.com>
2022-07-20 21:23:40 +0100

    testing deployment

--- connection.php
+++ connection.php
@@ -13,13 +13,13 @@ echo "Connection Successfully. Host info: " . $mysqlicon->host_info;
 //Get Heroku ClearDB connection information
 // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 // $cleardb_server = $cleardb_url["eu-cdbr-west-03.cleardb.net"];
-// $cleardb_username = $cleardb_url["b3b8a5d2b47e70"];
-// $cleardb_password = $cleardb_url["5f9c330e"];
+// $cleardb_username = $cleardb_url["b21d46832d533e"];
+// $cleardb_password = $cleardb_url["8f35cee4"];
 // $cleardb_db = substr($cleardb_url["path"],1);
 // $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$query_builder = TRUE;
+// Connect to DB
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 // echo "Hello there heroku"
 ?>
