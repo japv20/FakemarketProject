adbbe0678d4048d58ece3cbc4e1240115f88ce54
Jackeline <jackelineandrea58@hotmail.com>
2022-08-01 13:26:21 +0100

    icon

--- index.php
+++ index.php
@@ -36,7 +36,8 @@ $user_data = check_login($mysqlicon);
 <html>
 <head>
     <title> Fakemark </title>
-    <link rel="icon" href="../favicon.ico" type="image/ico">
+    <link rel="icon"  href="favicon.ico"/>
+    <!-- <link rel="icon" href="../favicon.ico" type="image/ico"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
