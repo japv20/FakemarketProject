a8716c8d2fc7c0f1179465be3fc7dcedf911e7ca
Jackeline <jackelineandrea58@hotmail.com>
2023-03-02 14:35:41 +0000

    updating

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ session_start();
     $_SESSION;
     
 // $mysqli = new mysqli("hostname", "username", "password", "database");
-// $mysqli = new mysqli("localhost", "root", "", "test");
-$mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "b21d46832d533e", "8f35cee4", "heroku_b2764aba82c3a92");
+$mysqli = new mysqli("localhost", "root", "", "test");
+// $mysqli = new mysqli("mongodb+srv", "japv20", "NYRKYclriLxwKiRc", "cluster0.kurlh4t.mongodb.net/test");
+// $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "b21d46832d533e", "8f35cee4", "heroku_b2764aba82c3a92");
  
 // Check connection
 if($mysqli === false){
